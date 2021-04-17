8da1266e276183f4e52ac5ae675521c783f477a2
MariosGen <mariosgen@athtech.gr>
2021-04-17 16:13:19 +0300

    minor changes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginPatientComponent } from './login/login-patient/login-patient.component';
 import { LoginDoctorComponent } from './login/login-doctor/login-doctor.component';
-import { RegisterDoctorComponent } from './register/register-doctor/register-doctor.component'
+import { RegisterDoctorComponent } from './register/register-doctor/register-doctor.component';
 import { RegisterPatientComponent } from './register/register-patient/register-patient.component';
 
 
