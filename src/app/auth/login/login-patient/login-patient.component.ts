1ea78406484b4047819a438c79cc5ce8db0835e3
MariosGen <mariosgen@athtech.gr>
2021-04-19 00:47:19 +0300

    minor fixed

--- src/app/auth/login/login-patient/login-patient.component.ts
+++ src/app/auth/login/login-patient/login-patient.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, NgForm, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-patient',
@@ -10,8 +10,34 @@ import { FormBuilder, NgForm, Validators } from '@angular/forms';
 
 export class LoginPatientComponent implements OnInit {
 
+  loginForm!: FormGroup;
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder) { }
+
+  validationMessages = {
+    'username' : {
+      'required': 'Username is required.',
+      'minlength': 'First name must be greater than 3 characters.',
+      'maxlength': 'First name must be less than 15 characters.'
+    },
+    'password' : {
+      'required': 'Password is required.',
+      'minlength': 'First name must be greater than 8 characters.',
+      'maxlength': 'First name must be less than 25 characters.'
+    }
+    
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required, Validators.minLength(4), Validators.maxLength(15)],
+      password: ['', Validators.required, Validators.minLength(8), Validators.maxLength(25),
+      Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]
+
+    })
+  }
+
+
 
 
   onSubmit(f: NgForm){
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -8,31 +8,5 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm!: FormGroup;
-
-  constructor(private fb: FormBuilder) { }
-
-  validationMessages = {
-    'username' : {
-      'required': 'Username is required.',
-      'minlength': 'First name must be greater than 3 characters.',
-      'maxlength': 'First name must be less than 15 characters.'
-    },
-    'password' : {
-      'required': 'Password is required.',
-      'minlength': 'First name must be greater than 8 characters.',
-      'maxlength': 'First name must be less than 25 characters.'
-    }
-    
-  }
-
-  ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      username: ['', Validators.required, Validators.minLength(4), Validators.maxLength(15)],
-      password: ['', Validators.required, Validators.minLength(8), Validators.maxLength(25),
-      Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]
-
-    })
-  }
-
+  ngOnInit(): void {}
 }
